0541f8143be61a3d77da5efd7a1f79e34bbc6f13
JavierFernandez136 <javier_fernandez_96@outlook.com>
2023-02-01 16:37:30 -0300

    hosteo con hiroku

--- db_conn.php
+++ db_conn.php
@@ -1,6 +1,6 @@
-<?php
+ <?php 
 
-$sname= "localhost";
+/* $sname= "localhost";
 
 $unmae= "root";
 
@@ -14,5 +14,15 @@ if (!$conn) {
 
     echo "Connection failed!";
 
-}
-
+}   */
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+ 
